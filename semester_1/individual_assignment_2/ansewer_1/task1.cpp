fbfe4092babed937838b0ca24449c3b52ecf8bde
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2025-10-16 21:32:10 +1000

    feat: ia2

---
+++ semester_1/individual_assignment_2/ansewer_1/task1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main() {
+    double a;
+    std::cin >> a;
+
+    if (a < 0) {
+        std::cerr << "Excepted that a grater than zero" << std::endl;
+        return 1;
+    }
+
+    const double v = pow(a, 3);
+    const double s = 6 * pow(a, 2);
+
+    std::cout
+        << "v = " << v << std::endl
+        << "s = " << s << std::endl;
+    return 0;
+}
---
+++ semester_1/individual_assignment_2/ansewer_1/task2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <fstream>
+
+int main() {
+    const char* filePath = "in2.dat";
+
+    std::ifstream stream(filePath);
+    if (stream.fail()) {
+        std::cerr << "Failed to open file: " << filePath << std::endl;
+        return 1;
+    }
+
+    double a;
+    stream >> a;
+    stream.close();
+
+    std::cout
+        << "Input:" << std::endl
+        << "a   = " << a << std::endl;
+
+    const double a2 = a * a;
+    const double a5 = a2 * a2 * a;
+    const double a17 = a5 * a5 * a5 * a2;
+
+    std::cout
+        << "Output:" << std::endl
+        << "a2  = " << a2 << std::endl
+        << "a5  = " << a5 << std::endl
+        << "a17 = " << a17 << std::endl
+        << std::endl;
+    return 0;
+}
