61dd6a05ad07f37fe187cff82231e3e2327fce93
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2025-10-22 02:39:42 +1000

    general_task_5

---
+++ semester_1/general_task_5/ansewer_1.1/task3.cpp
@@ -0,0 +1,46 @@
+﻿#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <cmath>
+
+int main() {
+    int pgrRes, eps;
+    long double x;
+
+    std::cin >> x >> eps;
+
+    long double a = 0;
+    if (eps > 0) {
+        if (fabsl(x) < 1) {
+            for (int n = 1; n < eps; n++) {
+                // Можно не использовать рекурентную формулу
+                // проверить делимость на 2 и записать знак
+                long double nPwr = 1;
+                for (int j = 1; j <= n + 1; j++) {
+                    nPwr *= -1;
+                }
+
+                long double xPwr = 1;
+                for (int j = 1; j <= n; j++) {
+                    xPwr *= x;
+                }
+
+                a += nPwr * (xPwr / n);
+            }
+
+            std::cout
+                << std::fixed << std::setprecision(std::numeric_limits<long double>::max_digits10)
+                << "ln(1 + x) = " << a << std::endl;
+            pgrRes = 0;
+        } else {
+            std::cerr << "x must be less than one" << std::endl;
+            pgrRes = 1;
+        }
+    } else {
+        std::cerr << "eps must be grater than zero" << std::endl;
+        pgrRes = 1;
+    }
+
+    return pgrRes;
+}
