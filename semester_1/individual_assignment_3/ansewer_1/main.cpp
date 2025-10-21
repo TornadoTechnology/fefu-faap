c59519b83fc618df7331583bc220d1983abe7214
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2025-10-22 01:03:13 +1000

    individual_assignment_3

---
+++ semester_1/individual_assignment_3/ansewer_1/main.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main() {
+    int pgrRes;
+    int a1, b1, c1, a2, b2, c2;
+    std::cin >> a1 >> b1 >> c1 >> a2 >> b2 >> c2;
+
+    if (a1 * b1 * c1 * a2 * b2 * c2 != 0) {
+        int result = 0;
+        if (a1 * b2 - a2 * b1 != 0 || a1 / a2 == b1 / b2 == c1 / c2) {
+            result = 1;
+        }
+
+        std::cout << "r = "<< result << std::endl;
+        pgrRes = 0;
+    } else {
+        std::cerr << "Input must be grater than zero" << std::endl;
+        pgrRes = 1;
+    }
+
+    return pgrRes;
+}
